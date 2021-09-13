8aa4644b14bce4ad9780e2ef358289fe115f8051
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-13 14:42:04 +0100

    test(*): fix previous tests

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,12 +2,11 @@ require 'game_of_life'
 
 describe 'game_of_life' do
   it 'returns an array when initialised' do
-    expect(initialise()).to eq Array
+    expect(initialise.class).to eq Array
   end
 
   it 'returns a 10 x 10 array filled with objects when initialised' do
-    expect(initialise()).to eq 
-    [
+    expect(initialise()).to eq [
       [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object],
       [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object],
       [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object],
