cadecd0720e567bdbba0a634d81f60d27da70cc9
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-13 17:07:15 +0100

    wip

--- lib/world.rb
+++ lib/world.rb
@@ -3,6 +3,9 @@ require_relative "cell"
 @grid = []
 
 def initialise(x, y)
+  @MAX_X = x
+  @MAX_Y = y
+  
   row = []
 
   (1..x).each do |a|
@@ -27,5 +30,49 @@ def display
   end
 end
 
-initialise(20,50)
-display
+def logic(x, y)
+  counter = 0
+
+
+  
+  # if @grid[x-1][y-1].alive == true
+  #   counter += 1 unless x - 1 < 0 || y - 1 < 0
+  # end
+  
+  # if @grid[x-1][y].alive == true
+  #   counter += 1 unless x - 1 < 0 || y - 1 < 0
+  # end
+  
+  # if @grid[x-1][y+1].alive == true
+  #   counter += 1 unless x - 1 < 0 || y - 1 < 0
+  # end
+  
+  # if @grid[x][y-1].alive == true
+  #   counter += 1 unless x - 1 < 0 || y - 1 < 0
+  # end
+  
+  # if @grid[x][y+1].alive == true
+  #   counter += 1
+  # end
+  
+  # if @grid[x+1][y-1].alive == true
+  #   counter += 1 unless x - 1 < 0 || y - 1 < 0
+  # end
+  
+  # if @grid[x+1][y].alive == true
+  #   counter += 1
+  # end
+  
+  # if @grid[x+1][y+1].alive == true
+  #   counter += 1
+  # end
+  
+  # puts counter
+end
+
+initialise(10,10)
+seed(0,9)
+logic(10,9)
+
+
+# display
