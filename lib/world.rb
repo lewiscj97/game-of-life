a5d27228c65037e53c526ca67e7a4188655837f0
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-14 10:20:31 +0100

    feat(world): convert to class

--- lib/world.rb
+++ lib/world.rb
@@ -1,73 +1,36 @@
 require_relative "cell"
 
-@grid = []
+class World
+  @@grid = []
+  @cells = []
 
-def initialise(x, y)
-  @MAX_X = x
-  @MAX_Y = y
-  
-  row = []
-
-  (0..x-1).each_with_index do |a, x_index|
-    (0..y-1).each_with_index do |b, y_index|
-      row << Cell.new(false, x_index, y_index)
-    end
-    @grid << row
+  def initialize(x, y)
     row = []
-  end
-end
-
-def seed(x, y)
-  @grid[x][y].alive = true
-end
-
-def display
-  @grid.each do |row|
-    row.each do |cell|
-      print cell.show
+  
+    (0..x-1).each_with_index do |a, x_index|
+      (0..y-1).each_with_index do |b, y_index|
+        row << Cell.new(false, x_index, y_index)
+      end
+      @@grid << row
+      row = []
     end
-    puts ""
+  end
+  
+  def seed(x, y)
+    @@grid[x][y].alive = true
+  end
+
+  def display
+    @@grid.each do |row|
+      row.each do |cell|
+        print cell.show
+      end
+      puts ""
+    end
   end
 end
 
-# This needs to be on the cell class
-# def logic(x, y)
-#   counter = 0
-#   if @grid[x-1][y-1].alive == true
-#     counter += 1 unless x - 1 < 0 || y - 1 < 0
-#   end
-  
-#   if @grid[x-1][y].alive == true
-#     counter += 1 unless x - 1 < 0 || y - 1 < 0
-#   end
-  
-#   if @grid[x-1][y+1].alive == true
-#     counter += 1 unless x - 1 < 0 || y - 1 < 0
-#   end
-  
-#   if @grid[x][y-1].alive == true
-#     counter += 1 unless x - 1 < 0 || y - 1 < 0
-#   end
-  
-#   if @grid[x][y+1].alive == true
-#     counter += 1
-#   end
-  
-#   if @grid[x+1][y-1].alive == true
-#     counter += 1 unless x - 1 < 0 || y - 1 < 0
-#   end
-  
-#   if @grid[x+1][y].alive == true
-#     counter += 1
-#   end
-  
-#   if @grid[x+1][y+1].alive == true
-#     counter += 1
-#   end
-  
-#   puts counter
-# end
-
-initialise(10,10)
-seed(0,9)
-seed(0,5)
+w = World.new(10,10)
+w.seed(0,9)
+w.seed(0,5)
+w.display
