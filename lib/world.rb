6d2310db4534f949ee8df3a11a7076b49495c5af
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-14 10:36:27 +0100

    feat(*): add cells to a flat array on initialisation

--- lib/world.rb
+++ lib/world.rb
@@ -2,18 +2,23 @@ require_relative "cell"
 
 class World
   @@grid = []
-  @cells = []
 
   def initialize(x, y)
     row = []
   
     (0..x-1).each_with_index do |a, x_index|
       (0..y-1).each_with_index do |b, y_index|
-        row << Cell.new(false, x_index, y_index)
+        cell = Cell.new(false, x_index, y_index)
+        row << cell
       end
       @@grid << row
       row = []
     end
+    @@cells = @@grid.flatten
+  end
+
+  def cells
+    @@cells
   end
   
   def seed(x, y)
@@ -34,3 +39,4 @@ w = World.new(10,10)
 w.seed(0,9)
 w.seed(0,5)
 w.display
+print w.cells
