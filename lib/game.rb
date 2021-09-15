6118bb93ad8d8ecb6ce178e30d018790d656ed5f
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-15 16:15:53 +0100

    feat(*): add glider

--- lib/game.rb
+++ lib/game.rb
@@ -5,12 +5,19 @@ class Game
   if ARGV.empty?
     w = World.new(20,20)
     w.build
+    w.seed_randomly
     w.run
   elsif ARGV[0] && ARGV[1]
     x = ARGV[0].to_i
     y = ARGV[1].to_i
     w = World.new(x, y)
     w.build
+    w.seed_randomly
+    w.run
+  elsif ARGV[0].downcase == "glider"
+    w = World.new(15,38)
+    w.build
+    w.seed_glider
     w.run
   else
     puts "Please enter two values"
