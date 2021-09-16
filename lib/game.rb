d6bb3a20a189bb46b02e5c3319c157d964477e95
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-16 12:53:37 +0100

    refactor(*): remove Glider constant from World class

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,38 @@
 require_relative 'cell'
 require_relative 'world_new'
+require_relative 'Constants'
 
 class Game
-  if ARGV.empty?
-    w = World.new(20,20)
-    w.build
-    w.seed_randomly
-    w.run
-  elsif ARGV[0] && ARGV[1]
-    x = ARGV[0].to_i
-    y = ARGV[1].to_i
-    w = World.new(x, y)
-    w.build
-    w.seed_randomly
-    w.run
-  elsif ARGV[0].downcase == "glider"
-    w = World.new(15,38)
-    w.build
-    w.seed_glider
-    w.run
-  else
-    puts "Please enter two values"
+  def self.main
+    if ARGV.empty?
+      w = World.new(20,20)
+      w.build
+      w.seed_randomly
+      w.run
+    elsif ARGV[0] && ARGV[1]
+      x = ARGV[0].to_i
+      y = ARGV[1].to_i
+      w = World.new(x, y)
+      w.build
+      w.seed_randomly
+      w.run
+    elsif ARGV[0].downcase == "glider"
+      w = World.new(15,38)
+      w.build
+      seed_glider(w)
+      w.run
+    else
+      puts "Please enter two values"
+    end
+  end
+
+  def self.seed_glider(w)
+    Constants.glider.each do |coordinate|
+      x, y = coordinate
+      w.seed(x,y)
+    end
   end
 end
 
-Game
+Game.main
+
